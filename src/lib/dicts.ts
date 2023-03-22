3e97edeba08fceca73c09b4bbc4732ec098b2f02
Your Name <you@example.com>
2023-03-22 10:19:25 +0800

    add filter select

---
+++ src/lib/dicts.ts
@@ -0,0 +1,5 @@
+export const USER_STATUS_DICT = {
+  '99': 'All',
+  '0': 'Paused',
+  '1': 'Active',
+};
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,15 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 import {
   EuiBasicTable,
+  EuiSelect,
   EuiPagination,
   EuiFlexGroup,
+  EuiFlexGrid,
   EuiFlexItem,
   EuiLink,
+  EuiSpacer,
+  EuiText,
 } from '@elastic/eui';
 import fetchData from '@/lib/fetch';
 import Navbar from '@/components/Navbar';
+import { USER_STATUS_DICT } from '@/lib/dicts';
 
 export async function getStaticProps({ params }) {
   return { props: {} };
@@ -20,12 +25,27 @@ interface Pagination {
   page_size: number;
 }
 
+interface UserFilterType {
+  status: string;
+}
+
 const Users: React.FC = () => {
   const router = useRouter();
+
   const [pagination, setPagination] = useState<Pagination>({
     page: 1,
     page_size: 10,
   });
+
+  const [filter, setFilter] = useState<UserFilterType>({
+    status: '99',
+  });
+
+  const paginationRef = useRef() as any;
+  paginationRef.current = pagination;
+
+  const filterRef = useRef() as any;
+  filterRef.current = filter;
 
   const [userList, setUserList] = useState<[]>([]);
   const [userCount, setUserCount] = useState<number>(0);
@@ -40,28 +60,36 @@ const Users: React.FC = () => {
       name: 'Name',
     },
     {
+      name: 'Status',
+      render: record => <span>{(USER_STATUS_DICT as any)[record.status]}</span>,
+    },
+    {
       name: 'Action',
       render: record => (
-        <EuiLink onClick={() => router.push(`/users/${record.id}`)}>
-          View
-        </EuiLink>
+        <span>
+          <EuiLink onClick={() => router.push(`/users/${record.id}`)}>
+            View
+          </EuiLink>
+        </span>
       ),
     },
   ];
 
+  const getUsers = async (pag, fil) => {
+    const fetchRes = await fetchData({
+      method: 'GET',
+      url: `/users`,
+      qs: {
+        ...pag,
+        ...fil,
+      },
+    });
+    setUserList(fetchRes.data.list);
+    setUserCount(fetchRes.data.count);
+  };
+
   useEffect(() => {
-    (async () => {
-      const fetchRes = await fetchData({
-        method: 'GET',
-        url: `/users`,
-        qs: {
-          ...pagination,
-        },
-      });
-      console.log(fetchRes);
-      setUserList(fetchRes.data.list);
-      setUserCount(fetchRes.data.count);
-    })();
+    getUsers(pagination, filter);
     // return () => {
     // }
   }, []);
@@ -74,6 +102,36 @@ const Users: React.FC = () => {
       <Navbar />
       <div className="page-container">
         <h1 className="page-header">User Table</h1>
+        <EuiFlexGrid columns={4}>
+          <EuiFlexItem>
+            <EuiText>Status</EuiText>
+            <EuiSelect
+              id="selectDocExample"
+              options={(Object.keys(USER_STATUS_DICT) as any).map(
+                (key: string) => {
+                  return { value: key, text: USER_STATUS_DICT[key] };
+                }
+              )}
+              value={filter.status}
+              onChange={e => {
+                const newFilter = {
+                  ...filter,
+                  status: e.target.value,
+                };
+                const newPagination = {
+                  page_size: pagination.page_size,
+                  page: 1,
+                };
+                setFilter(newFilter);
+                setPagination(newPagination);
+                getUsers(newPagination, newFilter);
+              }}
+              aria-label="Status"
+            />
+          </EuiFlexItem>
+        </EuiFlexGrid>
+
+        <EuiSpacer />
         <EuiBasicTable
           items={userList}
           rowHeader="id"
@@ -87,12 +145,14 @@ const Users: React.FC = () => {
             <EuiPagination
               pageCount={Math.floor(userCount / pagination.page_size) + 1}
               activePage={pagination.page}
-              onPageClick={targetPage =>
-                setPagination({
+              onPageClick={targetPage => {
+                const newPagination = {
                   page_size: pagination.page_size,
                   page: targetPage,
-                })
-              }
+                };
+                setPagination(newPagination);
+                getUsers(newPagination, filter);
+              }}
             />
           </EuiFlexItem>
         </EuiFlexGroup>
